75a50926ee33ed74d0e7f4da7ba53f31ff60bcab
ysjune <ysjune320@gmail.com>
2021-04-09 16:15:12 +0900

    initial commit

---
+++ src/test/java/com/example/shortening/ShorteningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shortening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShorteningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
